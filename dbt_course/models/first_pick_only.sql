3c08824517c8f6c752c0da2eee325498e8c70a7a
Ronen <you@example.com>
2025-08-11 19:09:10 +0300

    new uppdates

---
+++ dbt_course/models/first_pick_only.sql
@@ -0,0 +1,21 @@
+-- 1.Build a table named first_pick_only
+-- For each team and season, 
+-- show the team’s win percentage and round pick only 
+-- for their highest first-round draft pick that year (lowest round_pick).
+
+{{ config(materialized='table') }}
+
+
+with highest_pick as (
+
+select 
+    full_name,
+    season,
+    win_percentage,
+    round_pick,
+    row_number () over (partition by full_name,season order by round_pick asc) as RN
+ from {{ ref('win_precentage_draft_picks') }}
+ where round_pick is not null
+)
+select * from highest_pick
+where RN =1 
